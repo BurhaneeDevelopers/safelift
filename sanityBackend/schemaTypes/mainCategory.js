f183b6b92886802ee00c38fe5cce242a7cb8ae07
managetaheri <managetaheri@gmail.com>
2025-08-23 06:20:00 +0000

    Fixed sidebar not closing issue

--- sanityBackend/schemaTypes/mainCategory.js
+++ sanityBackend/schemaTypes/mainCategory.js
@@ -9,6 +9,30 @@ export default {
       title: 'Main Category Name',
     },
     {
+      title: 'Category Image',
+      name: 'categoryImage',
+      type: 'image',
+      options: {
+        hotspot: true, // <-- Defaults to false
+      },
+      fields: [
+        {
+          name: 'caption',
+          type: 'string',
+          title: 'Caption',
+          options: {
+            isHighlighted: true, // <-- make this field easily accessible
+          },
+        },
+        {
+          // Editing this field will be hidden behind an "Edit"-button
+          name: 'attribution',
+          type: 'string',
+          title: 'Attribution',
+        },
+      ],
+    },
+    {
       name: 'slug',
       type: 'slug',
       title: 'Slug',
